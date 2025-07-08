c15c03a2c4951aa2e9277cfc2b628770985aee9e
Christy Haragan <christy@TBN042.local>
2025-07-08 13:14:33 +0100

    yarg

---
+++ bad.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Banner from '@ddsweb/banner';
+
+const SimpleBanner2 = (props) => {
+	return <Banner
+		headline={props.title}
+
+		image={{
+			alt: props.title || ' ',
+	    	large: props.background.src,
+		}} />
+};
