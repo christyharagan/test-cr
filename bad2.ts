48bc2c59802bbeac912f64063fe84a71b3b1d197
Christy Haragan <christy@TBN042.local>
2025-07-09 09:33:58 +0100

    yarg

---
+++ bad2.ts
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import Banner from '@ddsweb/banner';
+
+const SimpleBanner2 = (props) => {
+	return <Banner
+		headline={props.title}
+		image={{ alt: 'yarg' || ' ', large: props.background.src,}}
+		/>
+};
