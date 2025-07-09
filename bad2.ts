31edfcf047d9ca509208b0f29feb05e12bdb15c9
Christy Haragan <christy@TBN042.local>
2025-07-09 11:18:12 +0100

    sdg

--- bad2.ts
+++ bad2.ts
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Banner from '@ddsweb/banner';
 
 const SimpleBanner2 = (props) => {
-    // return <Banner headline={props.title} image={{ alt: 'yarg' || ' ', large: props.background.src }} />
+    return <Banner headline={props.title} image={{ alt: 'yarg' || ' ', large: props.background.src }} />
 	// return <Banner
 	// 	headline={props.title}
 	// 	image={{ alt: 'yarg' || ' ', large: props.background.src,}}
