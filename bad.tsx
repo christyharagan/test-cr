0c0b7de62ad54a188ccbe43dd8841103d91513a7
Christy Haragan <christy@TBN042.local>
2025-07-09 11:14:03 +0100

    yarg

--- bad.tsx
+++ bad.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import Banner from '@ddsweb/banner';
 
 const SimpleBanner2 = (props) => {
-	return <Banner
-		headline={props.title}
+	return <Banner headline={props.title} image={{ alt: 'yarg' || ' ', large: props.background.src }} />
+	// return <Banner
+	// 	headline={props.title}
 
-		image={{
-			alt: props.title || ' ',
-	    	large: props.background.src,
-		}} />
+	// 	image={{
+	// 		alt: props.title || ' ',
+	//     	large: props.background.src,
+	// 	}} />
 };
